2ec86f53a72e33e6dbaa8e2cbb667f1f7eaf83cf
Rifqi Mukti Wicaksana <rifqimukti076@gmail.com>
2019-03-30 16:55:42 +0700

    Menambahkan fungsi doMainSweep, BFS, isFinished

--- main.cpp
+++ main.cpp
@@ -8,6 +8,63 @@ int isVisited[30][30];
 int size;
 queue <pair <int, int> > Queue;
 
+void BFS(int x, int y) {
+	pair <int,int> coord = make_pair(x,y);
+	Queue.push(coord);
+	
+	while(!Queue.empty()) {
+		coord = Queue.front();
+		if(isVisited[coord.first][coord.second] || board[coord.first][coord.second]=='X') {
+			Queue.pop();
+			continue;
+		}
+		int counter = 0; char tempcount;
+		
+		if(board[coord.first][coord.second-1]=='X') counter+=1; 
+		if(board[coord.first-1][coord.second-1]=='X') counter+=1; 
+		if(board[coord.first-1][coord.second]=='X') counter+=1; 
+		if(board[coord.first-1][coord.second+1]=='X') counter+=1; 
+		if(board[coord.first][coord.second+1]=='X') counter+=1; 
+		if(board[coord.first+1][coord.second+1]=='X') counter+=1; 
+		if(board[coord.first+1][coord.second]=='X') counter+=1; 
+		if(board[coord.first+1][coord.second-1]=='X') counter+=1; 
+		
+		if(counter>0) {
+			board[coord.first][coord.second] = (char)counter;
+		} else {
+			board[coord.first][coord.second] = '.';
+		}
+		
+		if(!isVisited[coord.first][coord.second-1] && board[coord.first][coord.second-1]!='X') Queue.push(make_pair(coord.first,coord.second-1));
+		if(!isVisited[coord.first-1][coord.second-1] && board[coord.first-1][coord.second-1]!='X') Queue.push(make_pair(coord.first-1,coord.second-1));
+		if(!isVisited[coord.first-1][coord.second] && board[coord.first-1][coord.second]!='X') Queue.push(make_pair(coord.first-1,coord.second));
+		if(!isVisited[coord.first-1][coord.second+1] && board[coord.first-1][coord.second+1]!='X' ) Queue.push(make_pair(coord.first-1,coord.second+1));
+		if(!isVisited[coord.first][coord.second+1] && board[coord.first][coord.second+1]!='X') Queue.push(make_pair(coord.first,coord.second+1));
+		if(!isVisited[coord.first+1][coord.second+1] && board[coord.first+1][coord.second+1]!='X') Queue.push(make_pair(coord.first+1,coord.second+1));
+		if(!isVisited[coord.first+1][coord.second] && board[coord.first+1][coord.second]!='X') Queue.push(make_pair(coord.first+1,coord.second));
+		if(!isVisited[coord.first+1][coord.second-1] && board[coord.first+1][coord.second-1]!='X' ) Queue.push(make_pair(coord.first+1,coord.second-1));
+		
+		isVisited[coord.first][coord.second] = 1;
+		Queue.pop();
+	}
+}
+
+
+bool isFinished() {
+	for(int i=0; i<size; i++) for(int j=0; j<size; j++) if(board[i][j]!='o') return 1;
+	return 0;
+}
+
+void doMineSweep() {
+	int x, y;
+	while(true) {	
+		print();
+		if(isFinished()) { printf("You win ?"); return; }
+		printf("Mau mencoba klik di koordinat (x,y) yang mana? : ");
+		scanf("%d %d", &x, &y);
+		BFS(x, y);
+	}	
+}
 
 int main() {
    int mines; 
@@ -27,6 +84,6 @@ int main() {
 		board[minePost.front().first][minePost.front().second] = 'X';
 		minePost.pop();
 	}
-	
+	doMineSweep();
 	return 0;
 }
