64407b848a161d78aed7987678404abc38babbff
fanlya <44891350+fanlya@users.noreply.github.com>
2019-03-30 16:43:40 +0700

    Update main.cpp
    salah tadi kwkwkwk

--- main.cpp
+++ main.cpp
@@ -10,6 +10,23 @@ queue <pair <int, int> > Queue;
 
 
 int main() {
+   int mines; 
+	printf("MINESWEEPER SEDERHANA\nInput nilai n untuk papan ukuran n x n yang diinginkan (maks 30): ");
+	scanf("%d", &size);
+	printf("Mau berapa mines (ranjau)? : ");
+	scanf("%d", &mines);
+	queue <pair <int,int> > minePost;
+	while(mines--) {
+		pair <int,int> minesLoc;
+		minesLoc.first = rand() % size;
+		minesLoc.second = rand() % size;
+		minePost.push(minesLoc);
+	}	
+	for(int i=0; i<size; i++) for(int j=0; j<size; j++) { board[i][j] = 'o'; isVisited[i][j] = 0; }
+	while(!minePost.empty()) {
+		board[minePost.front().first][minePost.front().second] = 'X';
+		minePost.pop();
+	}
 	
 	return 0;
 }
