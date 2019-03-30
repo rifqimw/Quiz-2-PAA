914a552e7b862d0f12c840c4caed8338bde8cdbd
fanlya <44891350+fanlya@users.noreply.github.com>
2019-03-30 16:38:49 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstdlib>
+#include <queue>
+using namespace std;
+
+char board[30][30];
+int isVisited[30][30];
+int size;
+queue <pair <int, int> > Queue;
+
+
+int main() {
+	
+	return 0;
+}
